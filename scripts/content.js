b0a4f780c83d4bcc4215a256fbdfccea01297383
FerryHm <69632149+FerryHm@users.noreply.github.com>
2024-08-29 23:44:46 +0200

    Works reliably now. Only thing left to do, is to improve performance, by making it calculate ii once the necessary elements are loaded (look into MutationObserver)

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,16 @@
-window.navigation.addEventListener("navigate", (event) => {
+console.log("starting extension...")
+setTimeout(function () {
+    console.log("executing...")
     ii();
+}, 2000)
+
+window.navigation.addEventListener("navigate", (event) => {
+    const regex = /\/users\/\d+\/osu$/;
+    if (regex.test(event.destination.url))
+        ii();
 })
 
 function ii() {
-    const url = window.location.href;
-
-    // Regular expression to check if the URL ends with /users/<id>/osu
-    const regex = /\/users\/\d+\/osu$/;
-
-    // Test the URL against the regular expression
-    if (regex.test(url))
-        return
-
     setTimeout(function () {
         let pp = 0;
         let playtime = 0;
@@ -19,62 +18,42 @@ function ii() {
         const labels = document.querySelectorAll('.value-display__label');
 
         labels.forEach(label => {
-            // Check if the label text is 'pp'
+            // Searches for pp, then gets playtime by sibling (bc pp is same in every language)
             if (label.textContent.trim() === 'pp') {
-                // Find the corresponding value element next to the label
-                const valueElement = label.nextElementSibling.querySelector('.value-display__value div');
+                const ppElement = label.nextElementSibling.querySelector('.value-display__value div');
+                const playtimeElement = ppElement.parentElement.parentElement.nextElementSibling.querySelector('.value-display__value span');
 
-                // Get the text content of the value element and remove commas
-                pp = valueElement.textContent.replace(/,/g, '');
-
-                // Convert to a number (optional)
-                pp = parseInt(pp, 10);
+                playtime = parseInt(playtimeElement.getAttribute('title').split(' ')[0].replace(',', ''));
+                pp = parseInt(ppElement.textContent.replace(/[,.]/g, ''));
             }
         });
 
-        labels.forEach(label => {
-            // Check if the label text is 'pp'
-            if (label.textContent.trim() === 'Total Play Time') {
-                // Find the corresponding value element next to the label
-                const valueElement = label.nextElementSibling.querySelector('.value-display__value span');
-                // Extract the 'data-orig-title' attribute value, which contains the total hours
-                const totalHours = valueElement.getAttribute('title');
-
-                // Parse the number of hours from the string
-                playtime = parseInt(totalHours.split(' ')[0].replace(',', ''));
-
-            }
-        });
-
-        // Compute expected playtime and the ratio
+        // Compute expected playtime and ii
         let expectedPlaytime = 1.16e-3 * Math.pow(pp, 1.17);
         let ii = expectedPlaytime / (playtime / 24);
 
+        // Insert ii on website
         updateElementStyles();
-        updateElementGap('10px');
+        updateElementGap('8px');
 
-        // Select a parent element to append the new element to
         const parentElement = document.querySelector('.profile-detail__values--grid');
 
         const outerDiv = document.createElement('div');
         outerDiv.className = 'value-display value-display--plain';
 
-        // Create the inner label div
         const labelDiv = document.createElement('div');
         labelDiv.className = 'value-display__label';
         labelDiv.textContent = 'ii';
 
-        // Create the inner value div
         const valueDiv = document.createElement('div');
         valueDiv.className = 'value-display__value';
         valueDiv.textContent = ii.toFixed(2) + "x";
 
-        // Append the label and value divs to the outer div
         outerDiv.appendChild(labelDiv);
         outerDiv.appendChild(valueDiv);
 
         parentElement.appendChild(outerDiv)
-    }, 1500)
+    }, 2000)
 }
 
 // Function to update grid-template-columns for elements with the class
