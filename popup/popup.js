00cfd1094c4bf618367472c667ee3d169819d67b
ferry <ferryemre@gmail.com>
2024-11-24 20:00:54 +0100

    mini commit

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const additionalPlaytimeHours = document.getElementById("additionalPlaytimeHours");
+    const goalpp = document.getElementById("goalpp");
 
     // Listen for when the user inputs a value
     additionalPlaytimeHours.addEventListener('change', async () => {
@@ -8,6 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
             const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
             const response = await chrome.tabs.sendMessage(tab.id, { additionalPlaytimeHours: additionalPlaytimeHours.value });
             console.log("sending playtime...");
+            console.log(response);
+        }
+    });
+
+    goalpp.addEventListener('change', async () => {
+        if (goalpp.value) {
+            const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+            const response = await chrome.tabs.sendMessage(tab.id, { goalpp: goalpp.value });
+            console.log("sending goalpp...");
             console.log(response);
         }
     });
